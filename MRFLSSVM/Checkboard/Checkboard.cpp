216af53d06ce46cd8ac981a2f909fddbb8ee823e
spacegoing <spacegoing@gmail.com>
2016-06-29 21:50:32 +1000

    C++ Version

---
+++ MRFLSSVM/Checkboard/Checkboard.cpp
@@ -0,0 +1,2 @@
+#include "Checkboard.h"
+
---
+++ MRFLSSVM/Checkboard/Checkboard.h
@@ -0,0 +1,47 @@
+//
+// Created by spacegoing on 6/29/16.
+//
+
+#ifndef CHECKBOARD_CHECKBOARD_H
+#define CHECKBOARD_CHECKBOARD_H
+
+#include <armadillo>
+#include <math.h>
+
+using namespace arma;
+
+struct Options {
+    // Image Configs
+    int gridStep = 16;  // grid size for defining cliques
+    int W = 128; // image width
+    int H = 128; // image height
+    int numCliques = (int) pow(W / gridStep, 2); // number of cliques
+    int N = W * H; // number of variables
+
+    // Learning Configs
+    int K = 4;  // number of lower linear functions
+    int maxIters = 100;  // maximum learning iterations
+    double eps = 1.0e-16;  // constraint violation threshold
+
+    // Other Configs
+    int learningQP = 1;  // encoding for learning QP (1, 2, or 3)
+    int figWnd = 0;  // figure for showing results
+};
+
+class Checkboard {
+public:
+    Options options;
+    Mat<int> cliques;
+    Mat<int> y;
+    cube unary;
+    cube pairwise;
+
+    Checkboard();
+    ~Checkboard();
+
+private:
+    
+
+};
+
+#endif //CHECKBOARD_CHECKBOARD_H
