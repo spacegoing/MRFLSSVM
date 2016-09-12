a6ffa44f3aef16fb484adf97a16a0c365c9b275b
spacegoing <spacegoing@gmail.com>
2016-09-12 12:18:38 +1000

    Added triad checkboard

--- PyMRFLSSVM/Checkboard.py
+++ PyMRFLSSVM/Checkboard.py
@@ -85,10 +85,66 @@ def checkboardHelper(eta1=0.1, eta2=0.1):
     return clique_indexes, y, unary_observed, pairwise
 
 
+# Generate checkboard data
+def checkboardHelper_triad(eta1=0.1, eta2=0.1):
+    _eta = [eta1, eta2]
+
+    H = Options.H
+    W = Options.W
+
+    # create checkboard data (clique and ground_truth y)
+    clique_indexes = np.zeros([H, W], dtype=np.int32, order='C')  # mapping of variables to clique_indexes
+    _cliqueID = 1.0  # clique index starts from 1
+    for _rowIndx in range(0, H, Options.gridStep):
+        for _colIndx in range(0, W, Options.gridStep):
+            clique_indexes[_rowIndx:_rowIndx + Options.gridStep,
+            _colIndx:_colIndx + Options.gridStep] = _cliqueID
+            _cliqueID += 1.0
+
+    y = np.zeros([H, W], dtype=np.int32, order='C')  # ground-truth labels
+    avg_no = H // 2
+
+    def line_seg(x, y):
+        return 1 if y - 2 * x > 0 else 0
+
+    for r in range(H):
+        for c in range(avg_no):
+            y[r, c] = line_seg(c, 127 - r)
+
+        y[r, avg_no:] = y[r, avg_no-1::-1]
+
+    # create unary features
+    unary_observed = np.zeros([H, W, 2], dtype=np.double, order='C')
+    for i in range(H):
+        for j in range(W):
+            unary_observed[i][j][1] = 2 * (np.random.rand(1, 1)[0, 0] - 0.5) + \
+                                      _eta[0] * (1 - y[i][j]) - _eta[1] * y[i][j]
+
+    # create pairwise features
+    pairwise = np.zeros([Options.rowsPairwise, 3], dtype=np.double, order='C')
+    if (Options.hasPairwise):
+        u = repmat(np.arange(W), H - 1, 1) * H + \
+            repmat(np.arange(H - 1).reshape([H - 1, 1]), 1, W)
+        v = repmat(np.arange(W - 1), H, 1) * H + \
+            repmat(np.arange(H).reshape([H, 1]), 1, W - 1)
+
+        for i, (u_i, v_i) in enumerate(zip(u.flatten('F'), v.flatten('F'))):
+            pairwise[i, 0] = u_i
+            pairwise[i, 1] = u_i + 1
+            pairwise[i + u.shape[0] * u.shape[1], 0] = v_i
+            pairwise[i + u.shape[0] * u.shape[1], 1] = v_i + H
+
+    return clique_indexes, y, unary_observed, pairwise
+
+
 class Instance:
     def __init__(self):
         # clique index starts from 1
-        self.clique_indexes, self.y, self.unary_observed, self.pairwise = checkboardHelper()
+
+        # self.clique_indexes, self.y, self.unary_observed, self.pairwise = checkboardHelper()
+
+        self.clique_indexes, self.y, self.unary_observed, self.pairwise = checkboardHelper_triad()
+
         self.latent_var = np.zeros([Options.numCliques, Options.K - 1])
         # self.unary = loadCheckboard().astype(np.double)
 
