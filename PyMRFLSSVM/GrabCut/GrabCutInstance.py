ee33163c0ea034118586a2887b689649c83e0fd1
spacegoing <spacegoing@gmail.com>
2016-09-24 23:09:39 +1000

    Finished GrabInstance

--- PyMRFLSSVM/GrabCut/GrabCutInstance.py
+++ PyMRFLSSVM/GrabCut/GrabCutInstance.py
@@ -6,12 +6,6 @@ import os
 
 __author__ = 'spacegoing'
 
-image_dir = './GrabCut/Data/grabCut/images/'
-
-mask_dir = './GrabCut/Data/grabCut/labels/'
-mask_ground_truth_type = ''
-mask_input_type = '_new'
-
 
 def get_name_path_arr(image_dir, mask_dir,
                       mask_ground_truth_type, mask_input_type):
@@ -36,19 +30,166 @@ def get_name_path_arr(image_dir, mask_dir,
     return name_image_mask_truemask
 
 
-name_image_mask_truemask = get_name_path_arr(image_dir, mask_dir,
-                                             mask_ground_truth_type, mask_input_type)
+class GrabCutInstance:
+    def __init__(self, image_path, mask_path, true_mask_path,
+                 method='quickshift', numSegments=200):
 
-image_name = name_image_mask_truemask[0, 0]
-image_path = name_image_mask_truemask[0, 1]
-mask_path = name_image_mask_truemask[0, 2]
-true_mask_path = name_image_mask_truemask[0, 3]
+        self.img_grabcut = GrabCut(image_path, mask_path)
 
-img_grabcut = GrabCut(image_path, mask_path)
+        self.input_mask = self.img_grabcut.input_mask
+        self.output_mask = self.img_grabcut.output_mask
+        self.true_mask = self.img_grabcut.read_mask_img(true_mask_path)
+        self.img = self.img_grabcut.img
 
-input_mask = img_grabcut.input_mask
-output_mask = img_grabcut.output_mask
-true_mask = img_grabcut.read_mask_img(true_mask_path)
+        self.unary_observed = self.get_unary_observed()
+        self.pairwise = self.get_pairwise(self.img)
+        self.cliques = self.get_cliques(image_path, method, numSegments)
 
-unary_observed = img_grabcut.get_unary_observed()
-cliques = superpixel(image_path)+1
+    def get_unary_observed(self):
+        return self.img_grabcut.get_unary_observed()
+
+    def get_cliques(self, image_path, method='quickshift', numSegments=200):
+        '''
+        Clique id starts from 1
+
+        :param image_path:
+        :return:
+        '''
+        if method == 'slic':
+            cliques = superpixel(image_path, method, numSegments=numSegments) + 1
+        else:
+            cliques = superpixel(image_path) + 1
+
+        return cliques
+
+    # calculate beta - parameter of grabcut algorithm.
+    # beta = 1/(2*avg(sqr(||color[i] - color[j]||)))
+    def calcbeta(self, img):
+        beta = 0
+        for y in range(img.shape[0]):
+            for x in range(img.shape[1]):
+
+                color = img[y, x, :]
+
+                if x > 0:  # left
+                    diff = color - img[y, x - 1]
+                    beta += diff.dot(diff)
+
+                if y > 0 and x > 0:  # upleft
+                    diff = color - img[y - 1, x - 1]
+                    beta += diff.dot(diff)
+
+                if y > 0:  # up
+                    diff = color - img[y - 1, x]
+                    beta += diff.dot(diff)
+
+                if y > 0 and (x < img.shape[1] - 1):  # upright
+                    diff = color - img[y - 1, x + 1]
+                    beta += diff.dot(diff)
+
+        if beta <= 1e-16:
+            beta = 0
+        else:
+            beta = 1.0 / (
+                2 * beta / (4 * img.shape[1] * img.shape[0] - 3 * img.shape[1] -
+                            3 * img.shape[0] + 2))
+
+        return beta
+
+    #   calculate weights of noterminal vertices of graph.
+    #   beta and gamma - parameters of grabcut algorithm.
+    def calcnweights(self, img, beta, gamma=50):
+        gammadivsqrt2 = gamma / np.sqrt(2.0)
+        leftw = np.zeros(img.shape[:2], dtype=np.double)
+        upleftw = np.zeros(img.shape[:2], dtype=np.double)
+        upw = np.zeros(img.shape[:2], dtype=np.double)
+        uprightw = np.zeros(img.shape[:2], dtype=np.double)
+
+        for y in range(img.shape[0]):
+            for x in range(img.shape[1]):
+
+                color = img[y, x, :]
+
+                if x > 0:  # left
+                    diff = color - img[y, x - 1]
+                    leftw[y, x] = gamma * np.exp(-beta * diff.dot(diff))
+                else:
+                    leftw[y, x] = 0.0
+
+                if y > 0 and x > 0:  # upleft
+                    diff = color - img[y - 1, x - 1]
+                    upleftw[y, x] = gammadivsqrt2 * np.exp(-beta * diff.dot(diff))
+                else:
+                    upleftw[y, x] = 0.0
+
+                if y > 0:  # up
+                    diff = color - img[y - 1, x]
+                    upw[y, x] = gamma * np.exp(-beta * diff.dot(diff))
+                else:
+                    upw[y, x] = 0.0
+
+                if y > 0 and (x < img.shape[1] - 1):  # upright
+                    diff = color - img[y - 1, x + 1]
+                    uprightw[y, x] = gammadivsqrt2 * np.exp(-beta * diff.dot(diff))
+                else:
+                    uprightw[y, x] = 0.0
+
+        return upleftw, leftw, upw, uprightw
+
+    def get_pairwise(self, img):
+        beta = self.calcbeta(img)
+        upleftw, leftw, upw, uprightw = self.calcnweights(img, beta)
+
+        pairwise_list = list()
+        current_indx = 0
+        cols = img.shape[1]
+        for y in range(img.shape[0]):
+            for x in range(img.shape[1]):
+
+                if x > 0:  # left
+                    pairwise_list.append([current_indx, current_indx - 1,
+                                          leftw[y, x]])
+
+                if y > 0 and x > 0:  # upleft
+                    pairwise_list.append([current_indx, current_indx - cols - 1,
+                                          upleftw[y, x]])
+
+                if y > 0:  # up
+                    pairwise_list.append([current_indx, current_indx - cols,
+                                          upw[y, x]])
+
+                if y > 0 and (x < img.shape[1] - 1):  # upright
+                    pairwise_list.append([current_indx, current_indx - cols + 1,
+                                          uprightw[y, x]])
+
+                current_indx += 1
+
+        pairwise = np.asarray(pairwise_list, dtype=np.double)
+
+        return pairwise
+
+
+if __name__ == '__main__':
+    image_dir = './GrabCut/Data/grabCut/images/'
+
+    mask_dir = './GrabCut/Data/grabCut/labels/'
+    mask_ground_truth_type = ''
+    mask_input_type = '_new'
+
+    name_image_mask_truemask = get_name_path_arr(image_dir, mask_dir,
+                                                 mask_ground_truth_type, mask_input_type)
+
+    i = 20
+    image_name = name_image_mask_truemask[i, 0]
+    image_path = name_image_mask_truemask[i, 1]
+    mask_path = name_image_mask_truemask[i, 2]
+    true_mask_path = name_image_mask_truemask[i, 3]
+
+    grabInstance = GrabCutInstance(image_path, mask_path, true_mask_path,
+                                   method='slic', numSegments=300)
+    print(grabInstance.img.shape)
+    print(np.max(grabInstance.unary_observed))
+    print(np.min(grabInstance.unary_observed))
+    print(np.unique(grabInstance.cliques))
+    print(np.max(grabInstance.pairwise))
+    print(grabInstance.pairwise.shape)
