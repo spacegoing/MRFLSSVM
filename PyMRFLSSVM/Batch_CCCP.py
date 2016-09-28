f13fcf49925da9772ca9e9689758fd056b3665a6
spacegoing <spacegoing@gmail.com>
2016-09-29 01:04:17 +1000

    Finished Batch Debug

--- PyMRFLSSVM/Batch_CCCP.py
+++ PyMRFLSSVM/Batch_CCCP.py
@@ -3,14 +3,11 @@ import numpy as np
 import matlab.engine
 import Batch_MRF_Helpers as mrf
 from MrfTypes import Example, Options
-from Utils.ReadMat import loadTestInf, loadMatPairwise
-from Utils.IOhelpers import dump_pickle
+import pickle
 
 __author__ = 'spacegoing'
 
 eng = matlab.engine.start_matlab()
-__DEBUG__ = 0
-__plot__ = 1
 
 
 def quadprog_matlab(P, q, A, b):
@@ -35,7 +32,8 @@ def cutting_plane_ssvm(theta, vt_list, examples_list, lossUnary_list, options):
     :param options:
     :type options: Options
     :return:
-    :rtype:
+    :rtype: np.ndarray
+    :rtype: list[dict]
     '''
     # theta: first 2K - 1 are higher order params, then unary, pairwise and slack
     # vt = Inferred Cutting Plane = truePhi
@@ -81,20 +79,12 @@ def cutting_plane_ssvm(theta, vt_list, examples_list, lossUnary_list, options):
         unaryWeight = theta[options.sizeHighPhi]
         pairwiseWeight = max(0, theta[options.sizeHighPhi + 1])
 
-        # todo: log data
-        y_hat, z_hat, e_hat = \
-            mrf.inf_label_latent_helper(unaryWeight * examples_list[0].unary_observed,
-                                        examples_list[0].pairwise,
-                                        examples_list[0].clique_indexes,
-                                        theta, options, examples_list[0].hasPairwise)
-        history.append({'theta': theta, 'y_hat': y_hat})
-
         loss_arr = np.zeros(examples_num)
         phi_loss_sum = np.zeros(options.sizePhi, dtype=np.double, order='C')
         violation_sum = 0.0
         for vt, ex, lossUnary, m in zip(vt_list, examples_list,
                                         lossUnary_list, range(examples_num)):
-            # todo: Errors: Unary features
+
             pairwise = np.copy(ex.pairwise)
             if ex.hasPairwise:
                 pairwise[:, 2] = pairwiseWeight * ex.pairwise[:, 2]
@@ -120,6 +110,8 @@ def cutting_plane_ssvm(theta, vt_list, examples_list, lossUnary_list, options):
         phi_loss = phi_loss_sum / examples_num
         A = np.r_[A, [np.r_[phi_loss, 1]]]
         b = np.r_[b, loss]
+
+        history.append({'theta': theta, 'loss_aug': loss})
 
         if all(theta_old == theta):
             print("break at %d" % t)
@@ -173,7 +165,7 @@ def cccp_outer_loop(examples_list, options, init_method='', inf_latent_method=''
     for ex in examples_list:
         lossUnary_list.append(mrf.augmented_loss(ex))
 
-    for t in range(10):
+    for t in range(50):
         theta_old = theta
 
         if inf_latent_method == 'remove_redundancy':
@@ -191,51 +183,50 @@ def cccp_outer_loop(examples_list, options, init_method='', inf_latent_method=''
         theta, inner_history = cutting_plane_ssvm(theta, vt_list, examples_list,
                                                   lossUnary_list, options)
 
-        total_error = np.sum(np.abs(examples_list[0].y - inner_history[-1]['y_hat']))
         outer_history.append({"inner_history": inner_history,
-                              "latent_inferred": latent_inferred_list[0],
-                              "total_error": total_error})
+                              "latent_inferred_list": latent_inferred_list})
+
+        with open('./expData/batchResult/temp/outer_iter%d.pickle' % t, 'wb') as f:
+            pickle.dump(outer_history, f)
 
         if all(theta == theta_old):
             print(latent_inferred_list[0])
             print('stop converge at iter: %d' % t)
-            print('classification error: %d' % total_error)
             break
 
         if np.sum(np.abs(examples_list[0].y - inner_history[-1]['y_hat'])) / examples_list[0].numVariables < \
                 options.eps:
             print(latent_inferred_list[0])
             print('converge at iter: %d' % t)
-            print('classification error: %d' % total_error)
             break
 
     return outer_history
 
 
 if __name__ == '__main__':
-    import numpy as np
-    import matlab.engine
-    from ReportPlots import plot_linfunc_converged, plot_colormap
-    import Batch_MRF_Helpers as mrf
-    from MrfTypes import Example, Options, BatchExamplesParser
-    from Utils.ReadMat import loadTestInf, loadMatPairwise
-    from Utils.IOhelpers import dump_pickle
-
-    __author__ = 'spacegoing'
-
-    eng = matlab.engine.start_matlab()
-    __DEBUG__ = 0
-    __plot__ = 1
-
-    # def quadprog_matlab(P, q, A, b):
-    #     P, q, A, b = [matlab.double(i.tolist())
-    #                   for i in [P, q, A, b]]
-    #     null = matlab.double([])
-    #     theta = eng.quadprog(P, q, A, b, null, null, null, null)
+    # import numpy as np
+    # import matlab.engine
+    # from ReportPlots import plot_linfunc_converged, plot_colormap
+    # import Batch_MRF_Helpers as mrf
+    # from MrfTypes import Example, Options, BatchExamplesParser
+    # from Utils.ReadMat import loadTestInf, loadMatPairwise
+    # from Utils.IOhelpers import dump_pickle
     #
-    #     return np.array(theta, dtype=np.double, order='C').reshape(len(theta))
-
-
+    # __author__ = 'spacegoing'
+    #
+    # eng = matlab.engine.start_matlab()
+    # __DEBUG__ = 0
+    __plot__ = 1
+    #
+    # # def quadprog_matlab(P, q, A, b):
+    # #     P, q, A, b = [matlab.double(i.tolist())
+    # #                   for i in [P, q, A, b]]
+    # #     null = matlab.double([])
+    # #     theta = eng.quadprog(P, q, A, b, null, null, null, null)
+    # #
+    # #     return np.array(theta, dtype=np.double, order='C').reshape(len(theta))
+    #
+    #
     # from Utils.IOhelpers import _load_grabcut_unary_pairwise_cliques
     # from MrfTypes import BatchExamplesParser
     #
@@ -243,10 +234,10 @@ if __name__ == '__main__':
     # parser = BatchExamplesParser()
     # examples_list = parser.parse_grabcut_pickle(raw_example_list)
     # options = Options
-
-    inf_latent_method = 'remove_redundancy'
-    init_method = 'clique_by_clique'
-
+    #
+    # inf_latent_method = ''
+    # init_method = 'clique_by_clique'
+    #
     # examples_list = [examples_list[0]]
     #
     # outer_history = cccp_outer_loop([examples_list[0]], options, inf_latent_method, init_method)
@@ -331,7 +322,6 @@ if __name__ == '__main__':
     # unaryWeight = theta[options.sizeHighPhi]
     # pairwiseWeight = max(0, theta[options.sizeHighPhi + 1])
     #
-    # # todo: log data
     # # if options.log_history:
     # #     y_hat, z_hat, e_hat = \
     # #         mrf.inf_label_latent_helper(unaryWeight * instance.unary_observed, instance.pairwise,
@@ -344,7 +334,6 @@ if __name__ == '__main__':
     # for vt, ex, lossUnary, m in zip(vt_list, examples_list,
     #                                 lossUnary_list, range(examples_num)):
     #     print(m)
-    #     # todo: Errors: Pairwise features
     #     if ex.hasPairwise:
     #         ex.pairwise[:, 2] = pairwiseWeight
     #
@@ -372,58 +361,50 @@ if __name__ == '__main__':
     #
     # theta = quadprog_matlab(P, q, -A, -b)
     # print("done theta")
+
+
+
+    # ########################## Checkboard Test ########################
+    # from Checkboard import Instance
     #
-    # ex.unary_observed.shape
-    # ex.unary_observed.dtype
-    # ex.pairwise.shape
-    # ex.pairwise.dtype
-    # ex.clique_indexes.shape
-    # ex.clique_indexes.dtype
-    # theta.shape
-    # theta.dtype
-
-
-    ########################## Checkboard Test ########################
-    from Checkboard import Instance
-
-    parser = BatchExamplesParser()
-    root = './expData/unbalaced_portions/'
-
-    # more black (1s)
-    prefix_str = "more_black_3339"
-    prefix_str = root + prefix_str
-
-    instance = Instance('gaussian_portions', portion_miu=(0.3, 0.3, 0.3, 0.9), is_gaussian=False)
-    examples_list = parser.parse_checkboard(instance)
-
-    options = Options()
-    outer_history = cccp_outer_loop([examples_list[0]], options, init_method, inf_latent_method)
-
-    dump_pickle(prefix_str, outer_history, instance, options)
-    plot_colormap(prefix_str, outer_history, instance, options)
-    plot_linfunc_converged(prefix_str, outer_history, options)
-
-    # more white (0s)
-    prefix_str = "more_white_1777"
-    prefix_str = root + prefix_str
-    instance = Instance('gaussian_portions', portion_miu=(0.1, 0.7, 0.7, 0.7), is_gaussian=False)
-    examples_list = parser.parse_checkboard(instance)
-
-    options = Options()
-    outer_history = cccp_outer_loop([examples_list[0]], options, init_method, inf_latent_method)
-    dump_pickle(prefix_str, outer_history, instance, options)
-    plot_colormap(prefix_str, outer_history, instance, options)
-    plot_linfunc_converged(prefix_str, outer_history, options)
-
-    prefix_str = "balanced_portions_124678"
-    prefix_str = root + prefix_str
-    instance = Instance('gaussian_portions',
-                        portion_miu=(0.1, 0.2, 0.4,
-                                     0.6, 0.7, 0.8), is_gaussian=False)
-    examples_list = parser.parse_checkboard(instance)
-
-    options = Options()
-    outer_history = cccp_outer_loop([examples_list[0]], options, init_method, inf_latent_method)
-    dump_pickle(prefix_str, outer_history, instance, options)
-    plot_colormap(prefix_str, outer_history, instance, options)
-    plot_linfunc_converged(prefix_str, outer_history, options)
+    # parser = BatchExamplesParser()
+    # root = './expData/unbalaced_portions/'
+    #
+    # # more black (1s)
+    # prefix_str = "more_black_3339"
+    # prefix_str = root + prefix_str
+    #
+    # instance = Instance('gaussian_portions', portion_miu=(0.3, 0.3, 0.3, 0.9), is_gaussian=False)
+    # examples_list = parser.parse_checkboard(instance)
+    #
+    # options = Options()
+    # outer_history = cccp_outer_loop([examples_list[0]], options, init_method, inf_latent_method)
+    #
+    # dump_pickle(prefix_str, outer_history, instance, options)
+    # plot_colormap(prefix_str, outer_history, instance, options)
+    # plot_linfunc_converged(prefix_str, outer_history, options)
+    #
+    # # more white (0s)
+    # prefix_str = "more_white_1777"
+    # prefix_str = root + prefix_str
+    # instance = Instance('gaussian_portions', portion_miu=(0.1, 0.7, 0.7, 0.7), is_gaussian=False)
+    # examples_list = parser.parse_checkboard(instance)
+    #
+    # options = Options()
+    # outer_history = cccp_outer_loop([examples_list[0]], options, init_method, inf_latent_method)
+    # dump_pickle(prefix_str, outer_history, instance, options)
+    # plot_colormap(prefix_str, outer_history, instance, options)
+    # plot_linfunc_converged(prefix_str, outer_history, options)
+    #
+    # prefix_str = "balanced_portions_124678"
+    # prefix_str = root + prefix_str
+    # instance = Instance('gaussian_portions',
+    #                     portion_miu=(0.1, 0.2, 0.4,
+    #                                  0.6, 0.7, 0.8), is_gaussian=False)
+    # examples_list = parser.parse_checkboard(instance)
+    #
+    # options = Options()
+    # outer_history = cccp_outer_loop([examples_list[0]], options, init_method, inf_latent_method)
+    # dump_pickle(prefix_str, outer_history, instance, options)
+    # plot_colormap(prefix_str, outer_history, instance, options)
+    # plot_linfunc_converged(prefix_str, outer_history, options)
