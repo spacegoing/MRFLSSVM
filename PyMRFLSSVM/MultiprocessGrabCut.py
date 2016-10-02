15f74f151feb4956bf07fe39737a39737d962ef3
spacegoing <spacegoing@gmail.com>
2016-10-03 02:13:18 +1100

    Added Dogs

--- PyMRFLSSVM/MultiprocessGrabCut.py
+++ PyMRFLSSVM/MultiprocessGrabCut.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import matplotlib
+
 matplotlib.use('Agg')
 from Batch_CCCP import cccp_outer_loop
 from Checkboard import Instance
 from MrfTypes import Example, Options, BatchExamplesParser
 from Utils.IOhelpers import _load_grabcut_unary_pairwise_cliques
+from Batch_MRF_Helpers import inf_label_latent_helper
 import multiprocessing
+import numpy as np
 import sys
 import pickle
 
@@ -87,6 +90,70 @@ def multip_grabCut():
     print('finished')
 
 
+def laipiDog(examples_list, leave_out_name, inf_latent_method, init_method, options, ex_test):
+    '''
+
+    :param ex_test:
+    :type ex_test: Example
+    :return:
+    :rtype:
+    '''
+
+    count = 0
+    loss = 1
+    outer_history = list()
+    while loss > 0.5:
+        outer_history = cccp_outer_loop(examples_list, options, inf_latent_method, init_method, leave_out_name)
+
+        with open('./expData/batchResult/training_result/'
+                  'laipi_leaveout_image_%s_%d_outer_history.pickle' % (leave_out_name, count), 'wb') as f:
+            pickle.dump([examples_list, outer_history, leave_out_name], f)
+
+        theta = outer_history['theta']
+        y_hat = inf_label_latent_helper(ex_test.unary_observed, ex_test.pairwise,
+                                        ex_test.clique_indexes, theta, options, ex_test.hasPairwise)
+        loss = np.sum(y_hat != ex_test.y) / (ex_test.y.shape[0] * ex_test.y.shape[1])
+        count += 1
+
+    with open('./expData/batchResult/training_result/'
+              'laipichenggong_leaveout_image_%s_%d_outer_history.pickle' % (leave_out_name, count), 'wb') as f:
+        pickle.dump([examples_list, outer_history, leave_out_name], f)
+
+
+def laipi_grabCut():
+    raw_example_list = _load_grabcut_unary_pairwise_cliques()
+    parser = BatchExamplesParser()
+    selected_num = 50
+    examples_list_all = parser.parse_grabcut_pickle(raw_example_list)
+    options = Options()
+
+    batches_num = len(examples_list_all)
+
+    inf_latent_method = ''
+    init_method = 'clique_by_clique'
+
+    laipi_name = ['tennis', 'person6', 'cross', '65019', 'elefant',
+                  'bool', '37073', 'banana1', '153077', 'book', '153093', '24077',
+                  '189080', '69020']
+
+    Tasks = list()
+    for i in range(selected_num):
+        ex_test = examples_list_all[i]
+        if ex_test.name.split('.')[0] in laipi_name:
+            examples_list = examples_list_all[:i] + examples_list_all[i + 1:]
+            Tasks.append((calcGrabCut, (examples_list, examples_list_all[i].name,
+                                        inf_latent_method, init_method, options)))
+    if len(Tasks) != len(laipi_name):
+        raise ValueError('Laipi doesn\'t match!')
+
+    pool = multiprocessing.Pool(len(laipi_name))
+    r = pool.map(calcFun, Tasks)
+    pool.close()
+    pool.join()
+    print(r)
+    print('finished')
+
+
 if __name__ == "__main__":
     multiprocessing.freeze_support()
 
@@ -94,7 +161,7 @@ if __name__ == "__main__":
 
     time_be = time.time()
 
-    multip_grabCut()
+    laipi_grabCut()
 
     time_end = time.time()
     m, s = divmod(time_end - time_be, 60)
